c5d6879f4e218656a6f41b5fb2d02a674e1c1d6f
한유경 <44183007+Begachu@users.noreply.github.com>
2024-03-12 17:08:56 +0900

    [Gold IV] Title: 여왕벌, Time: 616 ms, Memory: 115544 KB, Score: 83 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -3,10 +3,13 @@ input = sys.stdin.readline
 
 M, N = map(int, input().split())
 A = [[1]*M for _ in range(M)]
+S = [0 for i in range(M)]
 
 for _ in range(N):
-    _A = [[0]*M for _ in range(M)]
     grow = list(map(int, input().split()))
+
+    x0 = max(0, grow[0]-M)
+    x1 = max(0, grow[0]+grow[1]-M)
 
     for i in range(2*M-1):
         y = max(M-i-1, 0)
@@ -16,14 +19,18 @@ for _ in range(N):
             if grow[j]==0:
                 continue
             grow[j] -= 1
-            _A[y][x] = j
             A[y][x] += j
             break
     
-    for y in range(1, M):
-        for x in range(1, M):
-            _A[y][x] = max(_A[y][x-1],_A[y-1][x-1],_A[y-1][x])
-            A[y][x] += _A[y][x]
+    for i in range(x0+1, x1+1):
+        S[i] += 1
+    for i in range(x1+1, M):
+        S[i] += 2
+    
+
+for y in range(1, M):
+    for x in range(1, M):
+        A[y][x] += S[x]
 
 for y in range(M):
     print(" ".join(map(str, A[y])))
